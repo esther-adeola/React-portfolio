ac85eb5b77ccf30c103e5e3d8eed1381052d96c9
estherelujoba <estherelujoba@gmail.com>
2024-03-20 21:14:14 +0000

    update

--- challenge/myPortfolio/src/components/Resume.jsx
+++ challenge/myPortfolio/src/components/Resume.jsx
@@ -1,10 +1,10 @@
 ﻿// import React from 'react'
-
+import resume from "../images/CV_EE_SD.pdf"
 export default function Resume() {
   return (
     <>
     <h2>Please see Resume below</h2>
-    <iframe src="src/images/CV_EE SD.pdf" type="application/pdf" width="600" height="800"></iframe>
+    <iframe src={resume} type="application/pdf" width="600" height="800"></iframe>
     </>
   )
 }
